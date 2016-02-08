18209d58bbd6d139852486386104a5c379cc1d91
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-08 14:48:50 -0800

    fix: removed traces of api calls

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Twitter = require('twitter');
 var express = require('express');
 var keys = require('./config');
 var app = express();
-var port = 8300;
+var port = process.env.port || 8300;
 
 var twit = new Twitter({
   consumer_key: keys.TWITTER_CONSUMER_KEY,
@@ -18,32 +18,33 @@ app.get('/*', function(req, res) {
   res.send('Hello World');
 });
 
-twit.get('/statuses/mentions_timeline.json', { count: 10 },
-function(error, data) {
-  if (error) {
-    console.error(error);
-  }
-
-  var currentTweet;
-
-  for (var i = 0; i < data.length; i++) {
-    currentTweet = data[i];
-    if (!idStrings[currentTweet.id_str]) {
-      idStrings[currentTweet.ed_str] = true;
-      var tweetObj = {
-        user: currentTweet.user.screen_name,
-        text: currentTweet.text,
-      };
-      latestMentions.push(tweetObj);
-    }
-  }
-
-  console.log(idStrings);
-  console.log(latestMentions);
-});
-
 var server = app.listen(port, function() {
   console.log('Basic server listening on ' + port);
 });
 
-// API_KEY = '1Oas0q5vj3CqqZAj7xgLWYGdG';
+var getMentions = function() {
+  twit.get('/statuses/mentions_timeline.json', { count: 10 },
+  function(error, data) {
+    if (error) {
+      console.error(error);
+    }
+
+    for (var i = 0; i < data.length; i++) {
+      var currentTweet = data[i];
+      if (!idStrings[currentTweet.id_str]) {
+        idStrings[currentTweet.ed_str] = true;
+        var tweetObj = {
+          user: currentTweet.user.screen_name,
+          text: currentTweet.text,
+        };
+        latestMentions.push(tweetObj);
+      }
+    }
+
+    console.log(idStrings);
+    console.log(latestMentions);
+  });
+
+};
+
+getMentions();
