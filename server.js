bfe065e40e7a69199b65cabce363954af34923b7
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-09 13:36:27 -0800

    feat: tweet at anyone that mentions TGA

--- server.js
+++ server.js
@@ -22,47 +22,68 @@ var server = app.listen(port, function () {
   console.log('Basic server listening on ' + port);
 });
 
-var getMentions = function () {
-  twit.get('/statuses/mentions_timeline.json', { count: 10 },
-  function (error, data) {
-    if (error) {
-      console.error(error);
-    }
+var tweetForJs = function () {
+  twit.stream('statuses/filter', { track: 'TGA' }, function (stream) {
+    stream.on('data', function (tweet) {
+      var target = tweet.user.screen_name;
+      twit.post('statuses/update', { status: '@' + target + ' I like talking about how awesome TGA is too!' }, function (error, tweet) {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(tweet);
+        }
+      });
+    });
 
-    for (var i = 0; i < data.length; i++) {
-      var currentTweet = data[i];
-      if (!idStrings[currentTweet.id_str]) {
-        idStrings[currentTweet.ed_str] = true;
-        var tweetObj = {
-          user: currentTweet.user.screen_name,
-          text: currentTweet.text,
-        };
-        latestMentions.push(tweetObj);
-      }
-    }
+    stream.on('error', function (error) {
+      console.log(error);
+    });
+  });
+};
 
-    replyToMentions();
+tweetForJs();
+
+// var getMentions = function () {
+//   twit.get('/statuses/mentions_timeline.json', { count: 10 },
+//   function (error, data) {
+//     if (error) {
+//       console.error(error);
+//     }
+//
+//     for (var i = 0; i < data.length; i++) {
+//       var currentTweet = data[i];
+//       if (!idStrings[currentTweet.id_str]) {
+//         idStrings[currentTweet.id_str] = true;
+//         var tweetObj = {
+//           user: currentTweet.user.screen_name,
+//           text: currentTweet.text,
+//         };
+//         latestMentions.push(tweetObj);
+//       }
+//     }
+//
+//     replyToMentions();
 
     // console.log(idStrings);
     // console.log(latestMentions);
-  });
+  // });
 
-};
+// };
 
-var replyToMentions = function () {
-  for (var i = 0; i < latestMentions.length; i++) {
-    var currentMention = latestMentions[i];
-    var responseTweet = 'Hello @';
-    responseTweet += currentMention.user;
-    responseTweet += "\n what's poppin in the streets? \n-actual User...";
-    twit.post('statuses/update', { status: responseTweet }, function (error, tweet) {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log(tweet);
-      }
-    });
-  }
-};
+// var replyToMentions = function () {
+//   for (var i = 0; i < latestMentions.length; i++) {
+//     var currentMention = latestMentions[i];
+//     var responseTweet = 'Hello @';
+//     responseTweet += currentMention.user;
+//     responseTweet += "\n what's poppin in the streets? \n-actual User...";
+//     twit.post('statuses/update', { status: responseTweet }, function (error, tweet) {
+//       if (error) {
+//         console.error(error);
+//       } else {
+//         console.log(tweet);
+//       }
+//     });
+//   }
+// };
 
-getMentions();
+// getMentions();
