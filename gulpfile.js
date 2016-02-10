1ae628bb28e1f192ef6202b912857792e1aa6296
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-10 11:17:05 -0800

    feat: added basic front end files and frontend webpack config

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,46 @@ var gulp = require('gulp');
 var webpack = require('webpack');
 var path = require('path');
 var fs = require('fs');
+var DeepMerge = require('deep-merge');
+
+var deepmerge = DeepMerge(function (target, source, key) {
+  if (target instanceof Array) {
+    return [].concat(target, source);
+  }
+
+  return source;
+});
+
+//generic webpack.config
+
+var defaultConfig = {
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+    ],
+  },
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  defaultConfig.devtool = 'source-map';
+  defaultConfig.debug = true;
+}
+
+function config(overrides) {
+  return deepmerge(defaultConfig, overrides || {});
+}
+
+//Front-End
+
+var frontendConfig = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.join(__dirname, 'client/build'),
+    filename: 'frontend.js',
+  },
+};
+
+//Back-End
 
 var nodeModules = {};
 fs.readdirSync('node_modules')
@@ -12,29 +52,26 @@ fs.readdirSync('node_modules')
     nodeModules[mod] = 'commonjs ' + mod;
   });
 
-var config = {
-  entry: './src/server.js',
+var backendConfig = {
+  entry: './sever/server.js',
   target: 'node',
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'server/build'),
     filename: 'backend.js',
   },
-  externals: nodeModules,
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-    },
-  ],
+  node: {
+    __dirname: true,
+    __filename: true,
   },
+  externals: nodeModules,
   plugins: [
     new webpack.IgnorePlugin(/\.(css|less)$/),
     new webpack.BannerPlugin('require("source-map-support").install();',
       { raw: true, entryOnly: false }),
   ],
-  devtool: 'sourcemap',
 };
+
+//Tasks
 
 gulp.task('build-backend', function (done) {
   webpack(config).run(function (err, stats) {
