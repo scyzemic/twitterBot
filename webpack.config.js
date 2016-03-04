ab486cb3b87528f77df41692fab28c5d62937109
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-03-04 10:47:33 -0800

    maintenance: cleaning up repo

--- webpack.config.js
+++ webpack.config.js
@@ -4,74 +4,69 @@ var fs = require('fs');
 
 var nodeModules = {};
 fs.readdirSync('node_modules')
-  .filter(function (x) {
+  .filter(function(x) {
     return ['.bin'].indexOf(x) === -1;
   })
-  .forEach(function (mod) {
+  .forEach(function(mod) {
     nodeModules[mod] = 'commonjs ' + mod;
   });
 
-module.exports = [
-  {
-    name: 'frontend',
-    entry: [
-      'webpack-dev-server/client?http://localhost:8080',
-      'webpack/hot/only-dev-server',
-      './client/src/index.js'
-    ],
-    output: {
-      path: path.resolve(__dirname, 'build'),
-      publicPath: '/',
-      filename: 'frontend.js'
-    },
-    module: {
-      loaders: [
-        {
-          test:/\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'react-hot!babel'
-        },
-      ]
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    },
-    devServer: {
-      historyApiFallback: true,
-      contentBase: './client',
-      hot: true
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-    ],
-      devtool: 'sourcemap'
+module.exports = [{
+  name: 'frontend',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './client/src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'frontend.js'
   },
-  {
-    name: 'backend',
-    entry: [
-      './server/server.js'
-    ],
-    target: 'node',
-    output: {
-      path: path.resolve(__dirname, 'build'),
-      publicPath: '/',
-      filename: 'backend.js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js?$/,
-          exclude: /node_modules/,
-          loader: 'babel'
-        },
-      ]
-    },
-    externals: nodeModules,
-      plugins: [
-      new webpack.IgnorePlugin(/\.(css|less)$/),
-      new webpack.BannerPlugin('require("source-map-support").install();',
-                                { raw: true, entryonly: false }),
-    ],
-    devtool: 'sourcemap'
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }, ]
   },
-];
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './client',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devtool: 'sourcemap'
+}, {
+  name: 'backend',
+  entry: [
+    './server/server.js'
+  ],
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'backend.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, ]
+  },
+  externals: nodeModules,
+  plugins: [
+    new webpack.IgnorePlugin(/\.(css|less)$/),
+    new webpack.BannerPlugin('require("source-map-support").install();', {
+      raw: true,
+      entryonly: false
+    }),
+  ],
+  devtool: 'sourcemap'
+}, ];
